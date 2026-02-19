b2d8e7276d87e3c6c7bbfec67fc6ed3532c4b0b3
Ashis Shrestha <shrestha7.np@gmail.com>
2026-02-18 22:37:40 -0600

    Fixed Javadoc Comment

--- src/GroceryManagementSystem.java
+++ src/GroceryManagementSystem.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
 
+/**
+ * A grocery management system that demonstrates the use of parallel arrays to
+ * store item names, prices, and stock quantities.
+ *
+ * This program allows users to view inventory and restock items through a
+ * menu-driven interface.
+ *
+ * @author Puspa Raj Bhattarai
+ * @author Hayden Lovell
+ * @author Ashis Shrestha
+ */
 public class GroceryManagementSystem {
+
     /**
-    * Starts the program, initializes sample data, and handles
-    * user interaction through a looped menu.
-    * @author Puspa Raj Bhattarai
-    * @param args This is not used
-    */
+     * Initializes sample inventory data and runs the menu loop.
+     *
+     * @author Puspa Raj Bhattarai
+     * @param args Command-line arguments (not used)
+     */
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
@@ -15,7 +27,6 @@ public class GroceryManagementSystem {
         double[] itemPrices = new double[10];
         int[] itemStocks = new int[10];
 
-        
         itemNames[0] = "Eggs";
         itemPrices[0] = 3.5;
         itemStocks[0] = 10;
@@ -63,32 +74,38 @@ public class GroceryManagementSystem {
             }
         }
     }
-  public static void printInventory(String[] names, double[] prices, int[] stocks) {
-    /*
-    * Prints inventory to the terminal
-    * @author Hayden Lovell
-    * @param names An array of item names in the grocery store
-    * @param prices An array of item prices in the grocery store
-    * @param stocks An array of item stock amounts in the grocery store
-    */
-   
-    // Iterate through item names
-    for (int i = 0; i < names.length; i++) {
-      // Check if name exists
-      if (names[i] != null)
-        System.out.println("Name: " + names[i] + " | Price: " + prices[i] + " | Stock: " + stocks[i]);
-    }
-  }
 
     /**
-     * Searches for an item and restocks it by increasing its quantity by the specified amount.
+     * Prints inventory to the terminal
+     *
+     * @author Hayden Lovell
+     * @param names An array of item names in the grocery store
+     * @param prices An array of item prices in the grocery store
+     * @param stocks An array of item stock amounts in the grocery store
+     */
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+
+        // Iterate through item names
+        for (int i = 0; i < names.length; i++) {
+            // Check if name exists
+            if (names[i] != null) {
+                System.out.println("Name: " + names[i] + " | Price: " + prices[i] + " | Stock: " + stocks[i]);
+            }
+        }
+    }
+
+    /**
+     * Searches for an item and restocks it by increasing its quantity by the
+     * specified amount.
+     *
      * @author Ashis Shrestha
      * @param names An array of item names in the grocery store.
-     * @param stocks An array of stock quantities corresponding to the item names.
+     * @param stocks An array of stock quantities corresponding to the item
+     * names.
      * @param target The name of the item to be restocked.
      * @param amount The amount to restock the item by.
-    */
-    public static void restockItem(String[] names, int [] stocks, String target, int amount) {
+     */
+    public static void restockItem(String[] names, int[] stocks, String target, int amount) {
         int i = 0;
         boolean check = false;
         while (i < names.length) {
